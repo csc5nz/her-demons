9fb3b3664b45d16dea0102133b1c2fc3058b45f3
lki9419 <kl8jc@virginia.edu>
2017-10-26 18:12:22 -0400

    model rotate

--- New folder/Assets/Scripts/CameraControl.cs
+++ New folder/Assets/Scripts/CameraControl.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
+	public GameObject player;
+	public float cameraX = -75.0f;
+	public float cameraY = 75.0f;
+	public float cameraZ = -75.0f;
 	// Use this for initialization
 	void Start () {
-		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 pos = player.transform.position;
+		pos.x += cameraX;
+		pos.y += cameraY;
+		pos.z += cameraZ;
+
+		transform.position = pos;
 	}
 }
--- New folder/Assets/Scripts/PlayerControl.cs
+++ New folder/Assets/Scripts/PlayerControl.cs
@@ -5,36 +5,48 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour {
 
 	public float speed = 2.5F;
-	public string facing;
 
 	private Vector3 pos;
 	private Transform tr;
-	private bool movingX = false;
-	private bool movingZ = false;
+	private int imagePosition; 
+	private int newimagePosition;
 
 	// Use this for initialization
 	void Start () {
 		pos = transform.position;
 		tr = transform;
+		imagePosition = 0;
+		newimagePosition = 0;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetKey (KeyCode.D) && tr.position == pos) {
 			pos += 2*Vector3.back;
+			newimagePosition = 3;
 		}
 		if (Input.GetKey (KeyCode.A) && tr.position == pos) {
 			pos += 2*Vector3.forward;
+			newimagePosition = 1;
 		}
 		if (Input.GetKey (KeyCode.W) && tr.position == pos) {
 			pos += 2*Vector3.right;
+			newimagePosition = 0;
 		}
 		if (Input.GetKey (KeyCode.S) && tr.position == pos) {
 			pos += 2*Vector3.left;
+			newimagePosition = 2;
 		}
 
 		transform.position = Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed);
+		transform.Rotate (0, 90 * (imagePosition - newimagePosition), 0);
 
+		imagePosition = newimagePosition;
+	}
+
+	void rotateRight(){
+	}
+	void rotateLeft(){
 	}
 }
