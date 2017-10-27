ecb154729a37bcbb132b852bc4a538978ccd7940
lki9419 <kl8jc@virginia.edu>
2017-10-26 20:42:40 -0400

    update 842

--- New folder/Assets/Scripts/PlayerControl.cs
+++ New folder/Assets/Scripts/PlayerControl.cs
@@ -8,45 +8,56 @@ public class PlayerControl : MonoBehaviour {
 
 	private Vector3 pos;
 	private Transform tr;
-	private int imagePosition; 
-	private int newimagePosition;
+	private int faceDirection; 
+	private int newfaceDirection;
+	private bool can = true;
 
 	// Use this for initialization
 	void Start () {
 		pos = transform.position;
 		tr = transform;
-		imagePosition = 0;
-		newimagePosition = 0;
+		faceDirection = 0;
+		newfaceDirection = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		move (can);
 
+	}
+
+	public void elevatorup(){
+		Vector3 dest = transform.position + 4 * Vector3.up;
+		while(transform.position.y < dest.y ){
+			transform.position = Vector3.MoveTowards (transform.position, dest, Time.deltaTime * speed);
+		}
+
+	}
+
+	public void elevatordown(){
+	}
+
+	public void move(bool can){
 		if (Input.GetKey (KeyCode.D) && tr.position == pos) {
 			pos += 2*Vector3.back;
-			newimagePosition = 3;
+			newfaceDirection = 3;
 		}
 		if (Input.GetKey (KeyCode.A) && tr.position == pos) {
 			pos += 2*Vector3.forward;
-			newimagePosition = 1;
+			newfaceDirection = 1;
 		}
 		if (Input.GetKey (KeyCode.W) && tr.position == pos) {
 			pos += 2*Vector3.right;
-			newimagePosition = 0;
+			newfaceDirection = 0;
 		}
 		if (Input.GetKey (KeyCode.S) && tr.position == pos) {
 			pos += 2*Vector3.left;
-			newimagePosition = 2;
+			newfaceDirection = 2;
 		}
 
 		transform.position = Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed);
-		transform.Rotate (0, 90 * (imagePosition - newimagePosition), 0);
+		transform.Rotate (0, 90 * (faceDirection - newfaceDirection), 0);
 
-		imagePosition = newimagePosition;
-	}
-
-	void rotateRight(){
-	}
-	void rotateLeft(){
+		faceDirection = newfaceDirection;
 	}
 }
