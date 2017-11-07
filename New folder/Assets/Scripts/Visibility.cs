316738fdac7030a0f6ed41db43c7f5404009086a
lki9419 <kl8jc@virginia.edu>
2017-11-07 05:08:25 -0500

    visibility tweak

--- New folder/Assets/Scripts/Visibility.cs
+++ New folder/Assets/Scripts/Visibility.cs
@@ -7,10 +7,13 @@ public class Visibility : MonoBehaviour {
 	public GameObject player;
 	public bool hit;
 	public bool hit2;
+	public bool hit3;
 
 	private Vector3 two;
+	private Vector3 three;
 	private RaycastHit hitObject;
 	private RaycastHit hitObject2;
+	private RaycastHit hitObject3;
 
 	// Use this for initialization
 	void Start () {
@@ -19,11 +22,13 @@ public class Visibility : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		two = player.transform.position + 2.5f * Vector3.up;
+		three = player.transform.position + 1.25f * Vector3.up;
 		hit = Physics.Raycast (player.transform.position, new Vector3 (-75.0f, 75.0f, -75.0f), out hitObject, 50.0f);
 		hit2 = Physics.Raycast (two, new Vector3 (-75.0f, 75.0f, -75.0f), out hitObject2, 50.0f);
+		hit2 = Physics.Raycast (three, new Vector3 (-75.0f, 75.0f, -75.0f), out hitObject3, 50.0f);
 
 		if (hit) {
-			if (hitObject.collider.gameObject == this.gameObject || hitObject2.collider.gameObject == this.gameObject){
+			if (hitObject.collider.gameObject == this.gameObject || hitObject2.collider.gameObject == this.gameObject || hitObject3.collider.gameObject == this.gameObject){
 				this.gameObject.GetComponent<Renderer> ().material.shader = Shader.Find ("Transparent/Diffuse");
 				Color tempColor = this.gameObject.GetComponent<Renderer> ().material.color;
 				tempColor.a = 0.3F;
