d35cb0ebf71616fba4060323b3f18cbdf762f7fb
lki9419 <kl8jc@virginia.edu>
2017-12-03 02:07:07 -0500

    incomplete startin screen

---
+++ New folder/Assets/Scripts/menuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+	public Button start;
+	public Button controls;
+	public Button credits;
+	public Button exit;
+	public Canvas ctrlscreen;
+
+	// Use this for initialization
+	void Start () {
+		ctrlscreen.GetComponent<Canvas>();
+		start = start.GetComponent<Button> ();
+		start.onClick.AddListener (StartGame);
+		controls = controls.GetComponent<Button> ();
+		start.onClick.AddListener (ControlScreen);
+		credits = credits.GetComponent<Button> ();
+		exit = exit.GetComponent<Button> ();
+		exit.onClick.AddListener (ExitGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ControlScreen() {
+		ctrlscreen.enabled = true; 
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene (1);
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
