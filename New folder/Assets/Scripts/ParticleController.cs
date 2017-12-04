cd41c350f2712d0ab4df0fb295c76fe7901cc616
csc5nz <csc5nz@virginia.edu>
2017-12-04 02:02:40 -0500

    hpPotion particles

---
+++ New folder/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+	public GameObject player;
+	public ParticleSystem particleSystem;
+	private int prevHpPotion; 
+
+
+	void Awake ()
+	{
+		var main = particleSystem.main;
+		main.loop = true;
+		main.playOnAwake = false;
+	}
+
+	void Start ()
+	{	
+		particleSystem.enableEmission = false;
+		prevHpPotion = player.GetComponent<PlayerControl>().hpPotion;
+		transform.position = player.transform.position;
+		transform.parent = player.transform ;
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		print("prev" + prevHpPotion);
+		print("hp" + player.GetComponent<PlayerControl>().hpPotion);
+
+		if (player.GetComponent<PlayerControl>().hpPotion < prevHpPotion) {
+			print("if statement");
+			StartCoroutine(HpPotionEffect());
+			//prevHpPotion = player.GetComponent<PlayerControl> ().hpPotion;
+		}
+		prevHpPotion = player.GetComponent<PlayerControl> ().hpPotion;
+	}
+
+	IEnumerator HpPotionEffect() {
+		print(Time.time);
+		particleSystem.enableEmission = true;
+		yield return new WaitForSeconds (1.0f);
+		particleSystem.enableEmission = false;
+		print(Time.time);
+	}
+}
+
