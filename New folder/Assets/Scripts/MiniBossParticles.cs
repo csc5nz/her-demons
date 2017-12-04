2a437f590bfa423e73708d5d699df38e07e1e277
csc5nz <csc5nz@virginia.edu>
2017-12-04 03:12:37 -0500

    miniboss prefab

---
+++ New folder/Assets/Scripts/MiniBossParticles.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBossParticles : MonoBehaviour {
+	public GameObject miniboss;
+	public ParticleSystem particleSystem;
+	private int prevAnimatorInt; 
+
+	void Awake ()
+	{
+		var main = particleSystem.main;
+		main.loop = true;
+		main.playOnAwake = false;
+	}
+
+	void Start ()
+	{	
+		particleSystem.enableEmission = false;
+		prevAnimatorInt = miniboss.GetComponent<MinibossControl>().animator.GetInteger("enemymove");
+		transform.position = miniboss.transform.position;
+		transform.parent = miniboss.transform ;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//print("prev" + prevHpPotion);
+		//print("hp" + miniboss.GetComponent<MinibossControl>().hpPotion);
+		int curAnimatorInt = miniboss.GetComponent<MinibossControl>().animator.GetInteger("enemymove");
+		if (curAnimatorInt != prevAnimatorInt && curAnimatorInt == 4) {
+			print("if statement");
+			StartCoroutine(attackEffect());
+			//prevHpPotion = player.GetComponent<PlayerControl> ().hpPotion;
+		}
+		prevAnimatorInt = curAnimatorInt;
+	}
+
+	IEnumerator attackEffect() {
+		print(Time.time);
+		particleSystem.enableEmission = true;
+		yield return new WaitForSeconds (.5f);
+		particleSystem.enableEmission = false;
+		print(Time.time);
+	}
+}
